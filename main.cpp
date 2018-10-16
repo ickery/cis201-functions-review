224bcf7d9b69883a592c3c8c081c00a2ffac1a62
David Serrano <davidserrano@cis.vvc.edu>
2018-10-16 10:11:18 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
+/*
+File: main.cpp
+Developer: David Serrano
+Email: davidserrano@student.vvc.edu
+Description: Program that asks user for a string an return the amount of 'a's or 'A's in it.
+*/
+
 #include<iostream>
-
-
+#include<string>
+#include<ctype.h>
 using namespace std;
 
 
+// Checks a string for the amount of 'a's inside of it.
+// @param string the be checked.
+int count_a_in_string(string s)
+{
+    int count = 0;
+    for (int i = 0;i < s.size(); i++)
+    {
+        char character = tolower( s.at(i) );
+        if(character == 'a')
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
 int main()
 {
-    cout << "Hello World"<< endl;
-
+    string input;
+    cout << "Please type a string: ";
+    getline(cin, input);
+    cout << "There are " << count_a_in_string(input) << " letter 'a's in the string you typed." << endl;
     return 0;
 }
